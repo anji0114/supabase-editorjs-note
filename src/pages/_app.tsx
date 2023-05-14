ebe09f7886095b5b7acde8e571304b91128e23dd
anji0114 <anji.tnk.140114@gmail.com>
2023-05-14 12:28:34 +0900

    Meta情報の追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
 import { SessionContextProvider } from '@supabase/auth-helpers-react'
 import { useState } from 'react'
 import { SWRConfig } from 'swr/_internal'
-import Head from 'next/head'
+import { Meta } from '@/components/Meta'
 
 export default function App({ Component, pageProps }: AppProps) {
   const [supabaseClient] = useState(() => createBrowserSupabaseClient())
@@ -23,15 +23,7 @@ export default function App({ Component, pageProps }: AppProps) {
       supabaseClient={supabaseClient}
       initialSession={pageProps.initialSession}
     >
-      <Head>
-        <title>Note App</title>
-        <meta
-          name="description"
-          content="Next.js / Typescript / supabase / Editor.js を使用したノートアプリです"
-        />
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
+      <Meta />
       <SWRConfig value={{ fetcher }}>
         <Component {...pageProps} />
       </SWRConfig>
